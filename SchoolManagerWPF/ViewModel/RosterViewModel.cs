54f2524268b64d1472936c3db285cb7ee10034e3
TheKornelM <126570467+TheKornelM@users.noreply.github.com>
2024-11-20 22:01:24 +0100

    Show class roster (#6)

---
+++ SchoolManagerWPF/ViewModel/RosterViewModel.cs
@@ -0,0 +1,71 @@
+﻿using SchoolManagerModel.Entities;
+using SchoolManagerModel.Entities.UserModel;
+using SchoolManagerModel.Managers;
+using SchoolManagerModel.Persistence;
+using System.Collections.ObjectModel;
+
+namespace SchoolManagerWPF.ViewModel;
+
+public class RosterViewModel : ViewModelBase
+{
+    #region Public properties
+    public ObservableCollection<Class> Classes { get; set; } = [];
+    public ObservableCollection<User> CurrentClassRoster
+    {
+        get => _currentClassRoster;
+        set
+        {
+            // _currentClassRoster = value;
+            SetField(ref _currentClassRoster, value, nameof(CurrentClassRoster));
+        }
+
+    }
+    public Class? SelectedClass
+    {
+        get => _selectedClass;
+        set
+        {
+            if (_selectedClass == value || value == null)
+            {
+                return;
+            }
+
+            _selectedClass = value;
+            SetField(ref _selectedClass, value, nameof(SelectedClass));
+            GetClassRoster(_selectedClass);
+        }
+    }
+    #endregion
+
+    #region Private fields
+    private Class? _selectedClass;
+    private ObservableCollection<User> _currentClassRoster;
+    #endregion
+
+    #region Constructor
+    public RosterViewModel()
+    {
+        GetClasses();
+        SelectedClass = Classes.FirstOrDefault();
+    }
+    #endregion
+
+    #region Private methods
+    private async void GetClasses()
+    {
+        var schoolDbContext = new SchoolDbContext();
+        var classDatabase = new ClassDatabase(schoolDbContext);
+        var classManager = new ClassManager(classDatabase);
+        Classes = new ObservableCollection<Class>(await classManager.GetClassesAsync());
+    }
+
+    private async void GetClassRoster(Class cls)
+    {
+        SchoolDbContext schoolDbContext = new SchoolDbContext();
+        ClassDatabase classDatabase = new ClassDatabase(schoolDbContext);
+        ClassManager classManager = new ClassManager(classDatabase);
+        //CurrentClassRoster.Clear();
+        CurrentClassRoster = new ObservableCollection<User>(await classManager.GetClassStudentsAsync(cls));
+    }
+    #endregion
+}
