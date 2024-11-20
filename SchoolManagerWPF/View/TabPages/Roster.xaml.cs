55d92891608ac31b38b0a8b6b12ca98e675d75c5
TheKornelM <makari.kornel@gmail.com>
2024-11-20 21:58:35 +0100

    Show class roster

---
+++ SchoolManagerWPF/View/TabPages/Roster.xaml.cs
@@ -0,0 +1,25 @@
+﻿using SchoolManagerWPF.ViewModel;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace SchoolManagerWPF.View.TabPages;
+
+/// <summary>
+/// Interaction logic for Roster.xaml
+/// </summary>
+public partial class Roster : UserControl
+{
+    public Roster()
+    {
+        InitializeComponent();
+
+        // Skip viewmodel initialization in designer mode (to avoid designer crash)
+        if (DesignerProperties.GetIsInDesignMode(new DependencyObject()))
+        {
+            return;
+        }
+
+        RosterGrid.DataContext = new RosterViewModel();
+    }
+}
