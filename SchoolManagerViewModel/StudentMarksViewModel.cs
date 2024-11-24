7651fae7a9552684ce170609d8d90347e665f7b9
TheKornelM <makari.kornel@gmail.com>
2024-11-24 17:33:18 +0100

    Show marks
    Test version

---
+++ SchoolManagerViewModel/StudentMarksViewModel.cs
@@ -0,0 +1,36 @@
+﻿using SchoolManagerModel.Entities;
+using SchoolManagerModel.Entities.UserModel;
+using SchoolManagerModel.Managers;
+using SchoolManagerModel.Persistence;
+using System.Collections.ObjectModel;
+
+namespace SchoolManagerViewModel;
+
+public class StudentMarksViewModel(Student student) : ViewModelBase
+{
+    #region Private fields
+    private ObservableCollection<Mark> _marks = [];
+    #endregion
+
+    #region Public properties
+    public ObservableCollection<Mark> Marks
+    {
+        get => _marks;
+        set => SetField(ref _marks, value, nameof(Marks));
+    }
+    #endregion
+
+    #region Constructors
+    #endregion
+
+    #region Public methods
+    public async Task<List<Mark>> GetMarksAsync()
+    {
+        using var dbContext = new SchoolDbContext();
+        var subjectDatabase = new SubjectDatabase(dbContext);
+        var subjectManager = new SubjectManager(subjectDatabase);
+
+        return await subjectManager.GetStudentMarksAsync(student);
+    }
+    #endregion
+}
--- SchoolManagerWPF/View/StudentUI.xaml.cs
+++ SchoolManagerWPF/View/StudentUI.xaml.cs
@@ -1,4 +1,7 @@
-﻿using SchoolManagerModel.Entities.UserModel;
+﻿using SchoolManagerModel.Entities;
+using SchoolManagerModel.Entities.UserModel;
+using SchoolManagerViewModel;
+using System.Collections.ObjectModel;
 using System.Windows;
 
 namespace SchoolManagerWPF.View;
@@ -12,5 +15,41 @@ public partial class StudentUI : Window
     {
         InitializeComponent();
         Style = (Style)FindResource(typeof(Window));
+        GetViewModel2(student);
+    }
+
+    public async void GetViewModel2(Student student)
+    {
+        var viewmodel = new StudentMarksViewModel(student);
+        viewmodel.Marks = new ObservableCollection<Mark>(await GetMarksAsync(student));
+        StudentMarksControl.DataContext = viewmodel;
+    }
+
+    public async void SetViewModel(Student student)
+    {
+        var viewmodel = new StudentMarksViewModel(student);
+        viewmodel.Marks = new ObservableCollection<Mark>(await viewmodel.GetMarksAsync());
+        StudentMarksControl.DataContext = viewmodel;
+    }
+
+    public async Task<List<Mark>> GetMarksAsync(Student student)
+    {
+        var marks = new List<Mark>();
+        marks.Add(new Mark()
+        {
+            Grade = 5,
+            Student = student,
+            Subject = new Subject()
+            {
+                Class = student.Class,
+                Name = "Test",
+                Teacher = new Teacher() { User = student.User }
+            },
+            SubmitDate = DateTime.Today,
+            Notes = "Test"
+        });
+
+        await Task.Delay(500);
+        return marks;
     }
 }
