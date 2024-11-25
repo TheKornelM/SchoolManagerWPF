c3e8f8120e99a56cf34d1d41fe94df6260430e3a
TheKornelM <makari.kornel@gmail.com>
2024-11-25 09:42:33 +0100

    Updated mark viewmodel

---
+++ SchoolManagerViewModel/AddShowMarksViewModel.cs
@@ -0,0 +1,22 @@
+﻿using SchoolManagerModel.Entities;
+using SchoolManagerModel.Entities.UserModel;
+using System.Collections.ObjectModel;
+
+namespace SchoolManagerViewModel;
+
+public class AddShowMarksViewModel
+{
+    #region Private fields
+    #endregion
+
+    #region Public properties
+    public ObservableCollection<Subject> Subjects { get; set; } = [];
+    public ObservableCollection<Student> Students { get; set; } = [];
+    public bool IsSubjectSelected { get; set; } = false;
+    public bool IsStudentSelected { get; set; } = false;
+
+    public MarkViewModel Mark { get; set; } = new();
+    #endregion
+
+
+}
--- SchoolManagerViewModel/EntityViewModels/MarkViewModel.cs
+++ SchoolManagerViewModel/EntityViewModels/MarkViewModel.cs
@@ -1,20 +1,44 @@
 ﻿using SchoolManagerModel.Entities;
 using SchoolManagerModel.Entities.UserModel;
-using SchoolManagerViewModel;
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
 
-public class MarkViewModel : ViewModelBase
+namespace SchoolManagerViewModel
 {
-    [Key]
-    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-    public int Id { get; set; }
+    public class MarkViewModel : ViewModelBase
+    {
+        private int _grade;
+        private Student? _student;
+        private Subject? _subject;
+        private DateTime _submitDate;
+        private string _notes = string.Empty;
 
-    public int Grade { get; set; }
-    public required Student Student { get; set; }
-    public required Subject Subject { get; set; }
+        public int Grade
+        {
+            get => _grade;
+            set => SetField(ref _grade, value, nameof(Grade));
+        }
 
-    public required DateTime SubmitDate { get; set; }
-    public string Notes { get; set; } = string.Empty;
+        public Student? Student
+        {
+            get => _student;
+            set => SetField(ref _student, value, nameof(Student));
+        }
 
+        public Subject? Subject
+        {
+            get => _subject;
+            set => SetField(ref _subject, value, nameof(Subject));
+        }
+
+        public string Notes
+        {
+            get => _notes;
+            set => SetField(ref _notes, value, nameof(Notes));
+        }
+
+        public DateTime SubmitDate
+        {
+            get => _submitDate;
+            set => SetField(ref _submitDate, value, nameof(SubmitDate));
+        }
+    }
 }
